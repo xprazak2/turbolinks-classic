10224a35dc7c10144a7ec4cbb235c128fda6b247
Adam Becker <ad@mbecker.cc>
2016-09-19 15:28:41 -0700

    fix rails 5 deprecations

--- lib/turbolinks.rb
+++ lib/turbolinks.rb
@@ -11,8 +11,8 @@ module Turbolinks
       ActiveSupport.on_load(:action_controller) do
         ActionController::Base.class_eval do
           include XHRHeaders, Cookies, XDomainBlocker, Redirection
-          before_filter :set_xhr_redirected_to, :set_request_method_cookie
-          after_filter :abort_xdomain_redirect
+          before_action :set_xhr_redirected_to, :set_request_method_cookie
+          after_action :abort_xdomain_redirect
         end
 
         ActionDispatch::Request.class_eval do
@@ -25,7 +25,7 @@ module Turbolinks
 
       ActiveSupport.on_load(:action_view) do
         (ActionView::RoutingUrlFor rescue ActionView::Helpers::UrlHelper).module_eval do
-          include XHRUrlFor
+          prepend XHRUrlFor
         end
       end unless RUBY_VERSION =~ /^1\.8/
     end
